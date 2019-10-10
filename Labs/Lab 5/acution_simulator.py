e9c36dc7a1c401c4870f86b245a4e1b263d0cc01
Eric Zhang <ericweikaizhang@gmail.com>
2019-10-10 01:56:16 -0700

    Fix bidding logic

--- Labs/Lab 5/acution_simulator.py
+++ Labs/Lab 5/acution_simulator.py
@@ -9,12 +9,15 @@ class Auction:
         self.starting_price = starting_price
 
     def start_bid(self):
-        self.auctioneer = Auctioneer(self.bidders, self.starting_price, None)
-        largest_bidder_probability = 0
-        highest_current_bidder = None
-        highest_current_bid = 0
+
+        starting_bidder = Bidder('Starting Bid', 0, 0, 0, 0)
+        self.auctioneer = Auctioneer(self.bidders, self.starting_price, starting_bidder)
+
         bidding = True
         while bidding:
+            highest_current_bidder = starting_bidder
+            largest_bidder_probability = 0
+            highest_current_bid = 0
             bidding = False
             for bidder in self.bidders:
                 bidder(self.auctioneer)
@@ -26,18 +29,24 @@ class Auction:
                     highest_current_bidder = bidder
                     highest_current_bid = bidder.bid_increase_perc * \
                                           self.auctioneer.highest_current_bid
-            if self.auctioneer.highest_current_bidder is None:
-                start_bidder = 'Starting'
-            else:
-                start_bidder = self.auctioneer.highest_current_bidder.name
-            print(f'{highest_current_bidder.name} bidded {highest_current_bid} '
-                  f'in response to {start_bidder}\'s bid of '
-                  f'{self.auctioneer.highest_current_bid}')
+
+            bidders_enough_budget_list = [bidder.enough_budget for bidder in self.bidders]
+            if bidders_enough_budget_list.count(True) == 1 and \
+                    self.bidders[bidders_enough_budget_list.index(True)] \
+                    == self.auctioneer.highest_current_bidder:
+                break
+
+
+            if not bidding:
+                break
+            print(
+                f'{highest_current_bidder.name} bidded {highest_current_bid} '
+                f'in response to {self.auctioneer.highest_current_bidder.name}\'s bid of '
+                f'{self.auctioneer.highest_current_bid}!')
             self.auctioneer.accept_bid(highest_current_bid, highest_current_bidder)
         print(f'The winner of the auction is: '
               f'{self.auctioneer.highest_current_bidder.name} at '
               f'${self.auctioneer.highest_current_bid}!')
-
 
 
 class Auctioneer:
@@ -48,14 +57,13 @@ class Auctioneer:
         self.observers = []
 
     def accept_bid(self, bid, bidder):
-        #self.excute_callbacks()
+        # self.excute_callbacks()
         self.highest_current_bid = bid
         self.highest_current_bidder = bidder
 
     def execute_callbacks(self):
         for observer in self.observers:
             observer(self)
-
 
     def attach_observer(self, callback):
         self.observers.append(callback)
@@ -75,37 +83,36 @@ class Bidder:
         self.bid_probability = random.random()
 
     def __call__(self, auctioneer):
-        if auctioneer.highest_current_bidder is not None \
-                and self.name == auctioneer.highest_current_bidder.name:
-            print(f'{self.name} cant reliate myself')
-            self.bid_probability = 0
-        elif self.bid_increase_perc*auctioneer.highest_current_bid <= self.budget:
-            print(f'{self.name} has budget to bid')
-            self.update_bid_probability()
-        if self.bid_increase_perc*auctioneer.highest_current_bid > self.budget:
+        if self.bid_increase_perc * auctioneer.highest_current_bid > self.budget:
+            print(f'---{self.name} ran out of budget to bid')
             self.enough_budget = False
             self.bid_probability = 0
+        elif self.name == auctioneer.highest_current_bidder.name:
+            print(f'---{self.name} cant reliate myself')
+            self.bid_probability = 0
+        else:
+            print(f'---{self.name} has budget to bid')
+            self.update_bid_probability()
 
 
 def main():
-    #bid_item = input('Enter bid item: ')
+    # bid_item = input('Enter bid item: ')
     bid_item = 'iphone'
-    #starting_price = float(input('Enter starting price: '))
+    # starting_price = float(input('Enter starting price: '))
     starting_price = 100
-    #num_bidders = input('Enter number of bidders: ')
-    num_bidders = 2
+    # num_bidders = input('Enter number of bidders: ')
+    num_bidders = 3
     bidders = []
     for i in range(0, int(num_bidders)):
         name = input(f'Enter bidder {i + 1}\'s name: ')
-        #budget = float(input(f'Enter bidder {i + 1}\'s budget: '))
+        # budget = float(input(f'Enter bidder {i + 1}\'s budget: '))
         budget = 500
-        bid_increase_perc = float(input(f'Enter bidder {i + 1}\'s increase %: '))
+        bid_increase_perc = float(
+            input(f'Enter bidder {i + 1}\'s increase %: '))
         bidders.append(
             Bidder(name, budget, 0, bid_increase_perc, 0))
     auction = Auction(bidders, bid_item, starting_price)
     auction.start_bid()
-
-
 
 
 if __name__ == '__main__':
