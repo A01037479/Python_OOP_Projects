439a5e78e8c8f3fdfaca331ea83478c817d1af35
Eric Zhang <ericweikaizhang@gmail.com>
2019-10-09 18:23:27 -0700

    Add on last commit

--- Assignments/Assignment 2/membership_card.py
+++
@@ -1,7 +0,0 @@
-from card import Card
-
-
-class MembershipCard(Card):
-    def __init__(self, card_name, card_holder, issued_by, membership_level):
-        super().__init__(card_name, card_holder, issued_by)
-        self.membership_level = membership_level
